417c0dbb17ba1037ff250407637786949ec28dd2
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-09-02 23:46:51 +0500

    Question 88 Completed

---
+++ Question-88/main.js
@@ -0,0 +1,4 @@
+"use strict";
+// Make a function that convert string to number 
+const convert = (str) => Number(str);
+console.log(convert("123")); // 123
---
+++ Question-88/main.ts
@@ -0,0 +1,3 @@
+// Make a function that convert string to number 
+const convert = (str:string): number => Number(str);
+console.log(convert("123")); // 123
