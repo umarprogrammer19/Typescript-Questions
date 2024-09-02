774da7f986b3ffeff5f8e3a335959f1953f831f6
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-09-02 23:54:03 +0500

    Question 90 Completed

---
+++ Question-90/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// make a function that generates random number b/w 1-10
+const generate = () => console.log(Math.floor(Math.random() * 10) + 1);
+generate();
+generate();
+generate();
---
+++ Question-90/main.ts
@@ -0,0 +1,5 @@
+// make a function that generates random number b/w 1-10
+const generate = (): void => console.log(Math.floor(Math.random() * 10) + 1);
+generate();
+generate();
+generate();
