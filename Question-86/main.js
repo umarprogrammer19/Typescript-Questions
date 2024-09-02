ac7d6638814506e838fa04d2423b725d791cb245
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-09-02 23:37:31 +0500

    Question 86 Completed

---
+++ Question-86/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Check The Sentence includes "Javascriot" init
+const check = (str) => str.includes("Javascript");
+console.log(check("I Am Learning Javascript")); // True
+console.log(check("I Am Learning Tavascript")); // False
---
+++ Question-86/main.ts
@@ -0,0 +1,4 @@
+// Check The Sentence includes "Javascriot" init
+const check = (str: string): boolean => str.includes("Javascript");
+console.log(check("I Am Learning Javascript")); // True
+console.log(check("I Am Learning Tavascript")); // False
