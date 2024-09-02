4f57f296f4bd891d3c65b5dcc5b45436dee378d5
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-09-02 23:43:01 +0500

    Question 87 Completed

---
+++ Question-87/main.js
@@ -0,0 +1,4 @@
+"use strict";
+// make the function that takes out the first 10 characters from any text
+const takeOut = (str) => str.substring(0, 10);
+console.log(takeOut("I Have Completed My Assignments")); // Takeout 10 Letters
---
+++ Question-87/main.ts
@@ -0,0 +1,3 @@
+// make the function that takes out the first 10 characters from any text
+const takeOut = (str: string): string => str.substring(0, 10);
+console.log(takeOut("I Have Completed My Assignments")); // Takeout 10 Letters
