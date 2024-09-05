05754d7844566f73e8916999bcd096e47408b361
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-09-06 02:22:31 +0500

    Question 91 Completed

---
+++ Question-91/main.js
@@ -0,0 +1,5 @@
+"use strict";
+// Defines an array with three favorite fruits and push one fruit in it 
+let favoriteFruits = ["Apple", "Banana", "Cherry"];
+favoriteFruits.push("Mango"); // Adds "Mango" to the end of the array
+console.log(favoriteFruits); // Outputs: ['Apple', 'Banana', 'Cherry', 'Mango']
---
+++ Question-91/main.ts
@@ -0,0 +1,5 @@
+// Defines an array with three favorite fruits and push one fruit in it 
+let favoriteFruits: string[] = ["Apple", "Banana", "Cherry"];
+favoriteFruits.push("Mango"); // Adds "Mango" to the end of the array
+
+console.log(favoriteFruits); // Outputs: ['Apple', 'Banana', 'Cherry', 'Mango']
