4844da69d31e4c383f6f63588b2e786d4a142ab0
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-09-07 11:54:42 +0500

    Question 94 Completed

---
+++ Question-94/main.js
@@ -0,0 +1,5 @@
+"use strict";
+// Define array of words and find the length of all
+const words = ["Hello", "World", "TypeScript", "JavaScript"];
+const lengths = words.map(val => val.length);
+console.log(lengths);
---
+++ Question-94/main.ts
@@ -0,0 +1,6 @@
+// Define array of words and find the length of all
+const words: string[] = ["Hello", "World", "TypeScript", "JavaScript"];
+const lengths: number[] = words.map(val => val.length);
+console.log(lengths);
+
+
