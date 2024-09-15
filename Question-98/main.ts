49981c9696e8df000dbfd418a38fba21f687e87f
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-09-15 16:14:37 +0500

    Question 98 Complete

---
+++ Question-98/main.ts
@@ -0,0 +1,9 @@
+// Calculates how many days are left until New Year's Day
+function calculateDays(): number {
+    const today = new Date();
+    const newYear = new Date(today.getFullYear() + 1, 1, 0);
+    const diff = newYear.getTime() - today.getTime();
+    const days = Math.ceil(diff / (1000 * 60 * 60 * 24));
+    return days;
+}
+console.log(`There are ${calculateDays()} Days Left in a New Year`);
