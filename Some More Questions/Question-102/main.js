69e1a4ce380fb420fbb5f480a98eaca76a702903
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-11 06:48:49 +0500

    Question 102

---
+++ Some More Questions/Question-102/main.js
@@ -0,0 +1,4 @@
+"use strict";
+// Calculate Absolute Difference between -5 and 5
+const difference = Math.abs(-5 - 5);
+console.log(difference); // 10
---
+++ Some More Questions/Question-102/main.ts
@@ -0,0 +1,3 @@
+// Calculate Absolute Difference between -5 and 5
+const difference: number = Math.abs(-5 - 5);
+console.log(difference); // 10
