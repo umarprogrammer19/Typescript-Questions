7994f797439319ab34d1e7594f592f70e66b42ed
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-27 22:22:07 +0500

    Deleted 103

--- Some More Questions/Question-103/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// Make A Function Returns The Random Boolean;
-const generateRandomBoolean = () => Math.random() > 0.5;
-console.log(generateRandomBoolean());
-// Explanation: The Function Returns the condition That Math.random() > 0.5 It Means if Number is > 0.5 So It Returns True Else False... And Math.random() Generates Different Number between 0.1 - 0.9 In Every Run So The Boolean Value Is Also Change According To The random Number..
