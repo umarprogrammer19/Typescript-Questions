f4c870f14dcc560fd99dfacac078ccc071304951
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-28 22:24:10 +0500

    Deleted 102

--- Some More Questions/Question-102/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// Calculate Absolute Difference between -5 and 5
-const difference = Math.abs(-5 - 5);
-console.log(difference); // 10
--- Some More Questions/Question-102/main.ts
+++
@@ -1,3 +0,0 @@
-// Calculate Absolute Difference between -5 and 5
-const difference: number = Math.abs(-5 - 5);
-console.log(difference); // 10
